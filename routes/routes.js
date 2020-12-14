6a9708e0b2743e60e424452c851dacadfdea837c
Mohammad Yunus <mohammadyunus102@gmail.com>
2020-12-14 16:19:24 +0530

    update project

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,23 @@
 const router = require('express').Router();
 
 router.get('/', (req, res)=>{
-  res.status(200).send('Hi there')  
+  res.status(200).json('Hi there')  
+})
+
+router.get('/leader-board', (req, res)=>{
+    res.status(200).send("leader board")
+})
+
+router.get('/add-team', (req, res) => {
+    res.status(200).send("Add Team")
+})
+
+router.get('/select-teams', (req, res)=>{
+    res.send("select teams")
+})
+
+router.get('/search', (req, res)=>{
+    res.send("searching")
 })
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,4 +6,9 @@ const app = express();
 const Routes = require('./routes/routes')
 app.use('/', Routes)
 
+//Lets our server to accept json
+app.use(express.json())
+
+
+
 app.listen(process.env.PORT, ()=> console.log("Server Started"))
