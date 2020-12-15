20ad09b17a94197a0b05e21da9d8241fdf838be9
Mohammad Yunus <mohammadyunus102@gmail.com>
2020-12-15 19:14:14 +0530

    add team add functionality

--- routes/routes.js
+++ routes/routes.js
@@ -13,18 +13,18 @@ router.get('/leader-board', (req, res)=>{
 router.get('/add-team',async (req, res) => {
     const add_Team = new Hackers({
         team_name: req.query.team_name,
-        total_wins: 0,
-        total_losses: 0,
-        total_ties: 0,
-        total_score: 0
-    })
+        wins : 0,
+        losses : 0,
+        ties : 0,
+        scores : 0
+    });
     try{
-        const saved_team = await add_Team.save()
-        res.status(200).json(saved_team)
+        const new_team = await add_Team.save();
+        res.status(201).json(new_team)
     } catch (err){
         res.status(400).json(err)
     }
-})
+});
 
 router.get('/select-teams', (req, res)=>{
     res.send("select teams")
