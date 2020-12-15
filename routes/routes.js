4393241696cdcfaa6a617c089fa0e6d3d79dd17c
Mohammad Yunus <mohammadyunus102@gmail.com>
2020-12-15 22:02:44 +0530

    add won, loss, tied functionality

--- routes/routes.js
+++ routes/routes.js
@@ -26,8 +26,22 @@ router.get('/add-team',async (req, res) => {
     }
 });
 
-router.get('/select-teams', (req, res)=>{
-    res.send("select teams")
+router.get('/select-teams',async (req, res)=>{
+    const team_a = req.query.team_a;
+    const team_b = req.query.team_b;
+    const team_won = req.query.team_won;
+    const team_lost = req.query.team_lost;
+    const tie = req.query.tie;
+    
+    if(tie == 'true') {
+        const teamA = await Hackers.updateOne({ team_name: team_a }, { $inc : { ties : 1, scores: 1}});
+        const teamB = await Hackers.updateOne( {team_name: team_b}, {$inc: {ties: 1, scores: 1}} )
+        return res.status(200).json("Scores Updated")
+    }else{
+        await Hackers.updateOne({ team_name: team_won }, { $inc : { wins : 1, scores: 3}});
+        await Hackers.updateOne({ team_name: team_lost }, { $inc : { losses : 1}});
+        return res.status(500).send('Updated')
+    }
 })
 
 router.get('/search', (req, res)=>{
