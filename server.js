e86ce7d3ba67df3d33457a4b333e9aebf091e55a
Mohammad Yunus <mohammadyunus102@gmail.com>
2021-01-04 08:03:53 +0530

    deploy project

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
+const port = process.env.PORT || 1000;
 const app = express();
 
 //Setting view engine
